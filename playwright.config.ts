e0558648c157ba19c6444499beca7999290c84f5
adrian <146198705+tamburyn@users.noreply.github.com>
2025-08-18 09:21:27 +0200

    gitignore

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
   // Shared settings for all the projects below
   use: {
     // Base URL to use in actions like `await page.goto('/')`
-    baseURL: 'http://localhost:4321',
+    baseURL: 'http://localhost:3000',
     
     // Collect trace when retrying the failed test
     trace: 'on-first-retry',
@@ -70,7 +70,7 @@ export default defineConfig({
   // Run your local dev server before starting the tests
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:4321',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
